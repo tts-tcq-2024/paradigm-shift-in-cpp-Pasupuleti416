f88064a5fac56c4425bdf8558bfd0707e9b8981b
Pasupuleti416 <pasupuletiharitha68@gmail.com>
2024-09-05 09:32:10 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -49,16 +49,6 @@ bool batteryIsOk(float temperature, float soc, float chargeRate) {
   return temperatureIsOk(temperature);
   return socIsOk(soc);
   return chargeIsOk(chargeRate);
-  /*if(temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
-    return false;
-  } else if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  } else if(chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
-    return false;
-  }*/
   return true;
 }
 
